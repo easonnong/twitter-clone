7e2500a1272ca9548ced5b8f070c84a1fb5b6816
easonnong <nongeason@gmail.com>
2022-09-24 19:32:22 +0800

    add the input section for feed component

--- components/Feed/Input.js
+++ components/Feed/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Input = () => {
   return (
-    <div>
+    <div className="flex border-b border-gray-200 p-3 space-x-3">
       {/* Mini-Profile */}
       <img
         src="/images/user-icon.png"
@@ -11,17 +11,28 @@ export const Input = () => {
         className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
       ></img>
 
-      <div>
+      <div className="w-full">
         {/* Text area */}
-        <div>
-          <textarea rows="2" placeholder="What's happening?"></textarea>
+        <div className="divide-y divide-gray-200">
+          <textarea
+            rows="2"
+            placeholder="What's happening?"
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wider min-h-[50px] text-gray-700"
+          ></textarea>
         </div>
-        <div className="flex">
+        <div className="flex items-center justify-between">
           {/* Icon */}
-          <PhotoIcon className="h-7"></PhotoIcon>
-          <FaceSmileIcon className="h-7"></FaceSmileIcon>
+          <div className="flex">
+            <PhotoIcon className="h-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"></PhotoIcon>
+            <FaceSmileIcon className="h-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"></FaceSmileIcon>
+          </div>
           {/* Twitter Button */}
-          <button>Twitter</button>
+          <button
+            className="bg-blue-400 font-bold shadow-md hover:brightness-95 rounded-full text-white px-4 py-1.5 disabled:opacity-50"
+            disabled
+          >
+            Twitter
+          </button>
         </div>
       </div>
     </div>
