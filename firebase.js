8f890a00cbecc38f779b827743fc88eaed24aac4
easonnong <nongeason@gmail.com>
2022-09-27 08:11:20 +0800

    initialize firebase and next-auth

--- firebase.js
+++ firebase.js
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlszbDjvpBovJcJDxtCo7zhI6S0PePzmc",
-  authDomain: "twitter-clone-52946.firebaseapp.com",
-  projectId: "twitter-clone-52946",
-  storageBucket: "twitter-clone-52946.appspot.com",
-  messagingSenderId: "542656772601",
-  appId: "1:542656772601:web:9e33a971e995b4e0c5e528",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const store = getStorage();
+
+export { app, db, store };
