e1049aebcfdfe7ba915e0a9a2fb4c65dfa3d1427
easonnong <nongeason@gmail.com>
2022-09-27 09:07:56 +0800

    add signin

--- components/SignIn/index.js
+++ components/SignIn/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export const index = () => {
-  return <div>index</div>;
+export const Signin = (props) => {
+  return (
+    <div>
+      <img
+        src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
+        alt="twitter image inside a phone"
+        className="rotate-6 hidden object-cover md:w-44 md:h-80 md:inline-flex"
+      ></img>
+    </div>
+  );
 };
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,3 +1,19 @@
-export default function signin() {
-  return <div>signin</div>;
+import { Signin } from "../../components/SignIn";
+import { getProviders } from "next-auth/react";
+
+export default function signin(props) {
+  return (
+    <div>
+      <Signin props={props}></Signin>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
 }
