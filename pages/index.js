f0a82da46da85adf6fa6e17414f4130f9ff9112e
easonnong <nongeason@gmail.com>
2022-09-27 22:06:19 +0800

    add side bar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-
+import Head from "next/head";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Home() {
   return (
-  <div>Home</div>    
-  )
+    <div>
+      <Head>
+        <title>Twitter Clone</title>
+        <meta name="twitter clone" content="Created by eason nong" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body>
+        {/* Side bar */}
+        <Sidebar />
+
+        {/* Feed */}
+
+        {/* Widgets */}
+      </body>
+    </div>
+  );
 }
