f5d35e5d55810ffdcd90b668729360351255e125
easonnong <nongeason@gmail.com>
2022-09-30 15:45:27 +0800

    init

--- components/Sidebar/MenuIcon.js
+++
@@ -1,8 +0,0 @@
-export const MenuIcon = (props) => {
-  return (
-    <div className="flex pl-2 py-2 items-center space-x-2">
-      <props.icon className="h-7" />
-      <p className="text-gray-700">{props.text}</p>
-    </div>
-  );
-};
